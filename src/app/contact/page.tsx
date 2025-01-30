d124237746f97a1cb93f226799756a3a4dd71074
Munkhjavkhlan <munkhjavkhlann@gmail.com>
2025-01-30 13:52:31 +0800

    first commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Curve from "@/components/curve";
+
+export default function Page() {
+    return (
+        <Curve backgroundColor="#6E97D0">
+            <main>
+                <h1>contact</h1>
+            </main>
+        </Curve>
+    );
+}
